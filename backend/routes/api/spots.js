92ede45d21e7010c49883cb6bb2896592d7fe268
Jake Mann <JakeMann.com@gmail.com>
2024-10-29 06:02:07 -0700

    Get details of a Spot from an id

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { Spot, Review, SpotImage, User, sequelize } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth');
 
 const router = express.Router();
 
@@ -36,6 +37,86 @@ router.get('/', async (req, res) => {
   });
 
   return res.json({ Spots: spots });
+});
+
+// Get all spots owned by current user
+router.get('/current', requireAuth, async (req, res) => {
+    const spots = await Spot.findAll({
+      where: {
+        ownerId: req.user.id 
+      },
+      include: [
+        {
+          model: Review,
+          attributes: []
+        },
+        {
+          model: SpotImage,
+          attributes: [],
+          where: {
+            preview: true
+          },
+          required: false
+        }
+      ],
+      attributes: {
+        include: [
+          [
+            sequelize.fn('ROUND', sequelize.fn('AVG', sequelize.col('Reviews.stars')), 1),
+            'avgRating'
+          ],
+          [
+            sequelize.col('SpotImages.url'),
+            'previewImage'
+          ]
+        ]
+      },
+      group: ['Spot.id', 'SpotImages.url']
+    });
+  
+    return res.json({ Spots: spots });
+  });
+  
+// Get details of a spot from an id
+router.get('/:spotId', async (req, res) => {
+  const spot = await Spot.findByPk(req.params.spotId, {
+    include: [
+      {
+        model: SpotImage,
+        attributes: ['id', 'url', 'preview']
+      },
+      {
+        model: User,
+        as: 'Owner',
+        attributes: ['id', 'firstName', 'lastName']
+      },
+      {
+        model: Review,
+        attributes: []
+      }
+    ],
+    attributes: {
+      include: [
+        [
+          sequelize.fn('COUNT', sequelize.col('Reviews.id')),
+          'numReviews'
+        ],
+        [
+          sequelize.fn('ROUND', sequelize.fn('AVG', sequelize.col('Reviews.stars')), 1),
+          'avgStarRating'
+        ]
+      ]
+    },
+    group: ['Spot.id', 'SpotImages.id', 'Owner.id']
+  });
+
+  if (!spot) {
+    return res.status(404).json({
+      message: "Spot couldn't be found"
+    });
+  }
+
+  return res.json(spot);
 });
 
 module.exports = router;
